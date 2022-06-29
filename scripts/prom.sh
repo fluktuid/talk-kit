489de4318516de609c7cf39e29fadebda5b7d88b
Lukas Paluch <fluktuid@users.noreply.github.com>
2022-06-29 15:42:47 +0200

    update monitoring

--- scripts/prom.sh
+++ scripts/prom.sh
@@ -2,5 +2,4 @@ helm repo add prometheus-community https://prometheus-community.github.io/helm-c
 helm repo update
 helm upgrade --create-namespace --install prom prometheus-community/kube-prometheus-stack -n monitoring \
     --values prom.yml
-
-helm upgrade --install prom-adapter prometheus-community/prometheus-adapter --values prom-adapter.yml
+helm upgrade --install prom-adapter prometheus-community/prometheus-adapter --values prom-adapter.yml -n monitoring
